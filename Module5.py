9c8d9fef731aa2c22ab5ec9bac0623ac6b98e720
HarshitBhadani <harshitbhadani62@gmail.com>
2026-09-13 17:33:25 +0530

    adding small modification

--- Module5.py
+++ Module5.py
@@ -47,3 +47,5 @@ my_dog2 = Dog(name="Max")
 print(my_dog.eat())    # Buddy is eating
 print(my_dog.sleep())  # Buddy is sleeping
 print(my_dog.bark())   # Buddy says woof!
+
+print(my_dog2.sleep())  # Max is sleeping
